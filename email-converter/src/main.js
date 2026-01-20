af1f5118e5c87edc17f80e0e71346040390c7006
lunamoonmoon <5862lunamoon@gmail.com>
2026-01-20 14:14:28 -0800

    file drag and drop started

--- email-converter/src/main.js
+++
@@ -1,9 +0,0 @@
-import './style.css'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h3>Choose file to convert:</h3>
-    <input type="file" id="fileInput" />
-  </div>
-`
